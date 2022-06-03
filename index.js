e0ab2e1a7c850fe7d3f7fa4da4ec57bc6425dd10
atharvaBaste <msm19b024@iiitdm.ac.in>
2022-06-03 07:31:06 +0530

    added mongod

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyparser = require("body-parser")
 const expressvalidator = require('express-validator')
-
-
+const dotenv= require('dotenv')
+dotenv.config()
 //bring in routes
 const postroutes = require("./routes/post");
+const port = process.env.PORT;
 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log('DB connected'))
+
+mongoose.connection.on('error', err=>{
+  console.log(`DB connection error: ${err.message}`);
+
+})
  //middleware
   app.use(morgan("dev"));
   app.use(bodyparser.json());
-  app.use(expressvalidator);
+  // app.use(expressvalidator);
   app.use("/", postroutes);
 
 app.listen(port, () => {
